f63a9f3593eb8550dcdae006be98d38851a5f2ce
Dominic Michael Silveira <dms24081999@gmail.com>
2020-09-19 15:45:18 +0530

    Added BookParking Area Components with DateTimePicker

--- app/src/main/java/com/dominicsilveira/parkingsystem/NormalUser/BookParkingAreaActivity.java
+++ app/src/main/java/com/dominicsilveira/parkingsystem/NormalUser/BookParkingAreaActivity.java
@@ -2,42 +2,50 @@ package com.dominicsilveira.parkingsystem.NormalUser;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.ItemTouchHelper;
-import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
+import android.text.InputType;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
 import com.dominicsilveira.parkingsystem.R;
 import com.dominicsilveira.parkingsystem.classes.NumberPlate;
 import com.dominicsilveira.parkingsystem.classes.ParkingArea;
-import com.dominicsilveira.parkingsystem.utils.NumberPlateAdapter;
-import com.dominicsilveira.parkingsystem.utils.SimpleToDeleteCallback;
-import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import org.joda.time.DateTime;
+
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.Calendar;
 import java.util.List;
-import java.util.Map;
-import java.util.TreeMap;
 
 public class BookParkingAreaActivity extends AppCompatActivity {
     Spinner numberPlateSpinner;
+    TextView placeText,coordText,slotNoText;
+    TextView startBtn, endBtn;
+
     FirebaseAuth auth;
     FirebaseDatabase db;
 
-    List<String> numerPlateKeys = new ArrayList<String>();
-    List<String> numerPlateList = new ArrayList<String>();
+    DateTime startDateTime,endDateTime;
+
+    List<String> numberPlateKeys = new ArrayList<String>();
+    List<String> numberPlateList = new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,40 +55,103 @@ public class BookParkingAreaActivity extends AppCompatActivity {
         auth = FirebaseAuth.getInstance();
         db = FirebaseDatabase.getInstance();
 
-        numberPlateSpinner = findViewById(R.id.selectPlateNumber);
-        addItemsOnSpinner();
-        addListenerOnSpinnerItemSelection();
+        placeText = findViewById(R.id.placeText);
+        coordText = findViewById(R.id.coordText);
+        slotNoText = findViewById(R.id.slotNoText);
+        numberPlateSpinner = findViewById(R.id.vehicleSelect);
+        startBtn = findViewById(R.id.startBtn);
+        endBtn = findViewById(R.id.endBtn);
+        startBtn.setInputType(InputType.TYPE_NULL);
+        endBtn.setInputType(InputType.TYPE_NULL);
 
         Bundle bundle = getIntent().getExtras();
         String UUID=bundle.getString("UUID");
         final ParkingArea parkingArea = (ParkingArea) getIntent().getSerializableExtra("ParkingArea");
         Log.e("BookParkingAreaActivity",parkingArea.name+" "+UUID);
 
+        placeText.setText(parkingArea.name);
+        String coord=String.valueOf(parkingArea.latitude)+", "+String.valueOf(parkingArea.longitude);
+        coordText.setText(coord);
+
+        startBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showDateTIme(startBtn,false);
+            }
+        });
+
+        endBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showDateTIme(endBtn,true);
+            }
+        });
+
+        addItemsOnSpinner();
+        addListenerOnSpinnerItemSelection();
     }
+
+    private void showDateTIme(final TextView button, final boolean end) {
+        final Calendar calendar=Calendar.getInstance();
+        DatePickerDialog.OnDateSetListener dateSetListener=new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, final int date) {
+                calendar.set(Calendar.YEAR,year);
+                calendar.set(Calendar.MONTH,month);
+                calendar.set(Calendar.DAY_OF_MONTH,date);
+
+                TimePickerDialog.OnTimeSetListener timeSetListener= new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker timePicker, int hour, int minute) {
+                        calendar.set(Calendar.HOUR_OF_DAY,hour);
+                        calendar.set(Calendar.MINUTE,minute);
+                        SimpleDateFormat simpleDateFormat=new SimpleDateFormat("yyyy-MM-dd HH:mm");
+                        button.setText(simpleDateFormat.format(calendar.getTime()));
+                        if(end){
+                            endDateTime = new DateTime(calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH), calendar.get(Calendar.HOUR_OF_DAY), calendar.get(Calendar.MINUTE), 0,0);
+                        }else{
+                            startDateTime = new DateTime(calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH), calendar.get(Calendar.HOUR_OF_DAY), calendar.get(Calendar.MINUTE), 0,0);
+                        }
+                        if(endDateTime!=null && startDateTime!=null){
+                            if(endDateTime.getMillis()>startDateTime.getMillis()){
+                                Toast.makeText(BookParkingAreaActivity.this,
+                                        "after", Toast.LENGTH_SHORT).show();
+                            }else{
+                                Toast.makeText(BookParkingAreaActivity.this,
+                                        "before", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    }
+                };
+                TimePickerDialog timePickerDialog=new TimePickerDialog(BookParkingAreaActivity.this,timeSetListener,calendar.get(Calendar.HOUR_OF_DAY),calendar.get(Calendar.MINUTE),false);
+                timePickerDialog.show();
+            }
+        };
+        DatePickerDialog datePickerDialog=new DatePickerDialog(BookParkingAreaActivity.this,dateSetListener,calendar.get(Calendar.YEAR),calendar.get(Calendar.MONTH),calendar.get(Calendar.DAY_OF_MONTH));
+        datePickerDialog.getDatePicker().setMinDate(System.currentTimeMillis() - 1000);
+        datePickerDialog.show();
+    }
+
     public void addItemsOnSpinner() {
-        numerPlateKeys.add("0");
-        numerPlateList.add("Select a vehicle");
-//        list.add("list 1");
-//        list.add("list 2");
-//        list.add("list 3");
+        numberPlateKeys.add("0");
+        numberPlateList.add("Select a vehicle");
         db.getReference().child("NumberPlates").orderByChild("userID").equalTo(auth.getCurrentUser().getUid())
                 .addListenerForSingleValueEvent(new ValueEventListener() {
                     @Override
                     public void onDataChange(@NonNull DataSnapshot snapshot) {
                         for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
                             NumberPlate numberPlate = dataSnapshot.getValue(NumberPlate.class);
-                            numerPlateKeys.add(dataSnapshot.getKey());
-                            numerPlateList.add(numberPlate.numberPlate);
+                            numberPlateKeys.add(dataSnapshot.getKey());
+                            numberPlateList.add(numberPlate.numberPlate);
                         }
                         ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(BookParkingAreaActivity.this,
-                                android.R.layout.simple_spinner_item, numerPlateList);
+                                android.R.layout.simple_spinner_item, numberPlateList);
                         dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
                         numberPlateSpinner.setAdapter(dataAdapter);
                     }
                     @Override
                     public void onCancelled(@NonNull DatabaseError error) {}
                 });
-
     }
 
     public void addListenerOnSpinnerItemSelection() {
@@ -90,13 +161,8 @@ public class BookParkingAreaActivity extends AppCompatActivity {
                 if(position!=0)
                 Toast.makeText(BookParkingAreaActivity.this, String.valueOf(numberPlateSpinner.getSelectedItem())+String.valueOf(position), Toast.LENGTH_SHORT).show();
             }
-
             @Override
-            public void onNothingSelected(AdapterView<?> adapterView) {
-
-            }
+            public void onNothingSelected(AdapterView<?> adapterView) {}
         });
-
-
     }
 }
